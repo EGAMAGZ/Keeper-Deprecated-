1ca19a1a01018f4ee5579877654b903301c8c55c
EGAMAGZ <gazapito1@hotmail.com>
2019-09-13 17:24:43 -0500

    Added the method to saveTasks[WORKING]

--- app/src/main/java/com/android/keeper/dialog/AddNewTaskBottomSheet.java
+++ app/src/main/java/com/android/keeper/dialog/AddNewTaskBottomSheet.java
@@ -1,5 +1,6 @@
 package com.android.keeper.dialog;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,25 +8,31 @@ import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.keeper.R;
+import com.android.keeper.localdb.SQLiteConnection;
+import com.android.keeper.localdb.utilities.TasksUtilities;
 
 public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
 
     private View bottomSheetView;
     private ImageButton addDetailsButton,saveTaskButton;
     private EditText titleEditText,descriptionEditText;
+    private SQLiteConnection conn;
 
-    private String task_title,task_description;
+    private String task_title,task_details;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         bottomSheetView=inflater.inflate(R.layout.bottom_sheet_add_new_task,container,false);
-        Toast.makeText(getContext(),"Add New Task",Toast.LENGTH_SHORT).show();
+        conn=new SQLiteConnection(getContext(),"keeper_db",null,1);
 
         addDetailsButton=bottomSheetView.findViewById(R.id.task_add_description);
         saveTaskButton=bottomSheetView.findViewById(R.id.task_save);
@@ -50,12 +57,13 @@ public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
             public void onClick(View view) {
                 task_title=titleEditText.getText().toString();
                 if(descriptionEditText.getVisibility()== View.VISIBLE){
-                    task_description=descriptionEditText.getText().toString();
-                }else{ task_description="";}
+                    task_details=descriptionEditText.getText().toString();
+                }else{ task_details="";}
 
                 if(task_title.isEmpty()){
                     Toast.makeText(getContext(),"Task Title is Empty",Toast.LENGTH_SHORT).show();
                 }else{
+                    saveTask();
                     Toast.makeText(getContext(),"Task will be saved",Toast.LENGTH_SHORT).show();
                     dismiss();
                 }
@@ -64,5 +72,12 @@ public class AddNewTaskBottomSheet extends BottomSheetDialogFragment {
 
 
         return bottomSheetView;
+    }
+    private void saveTask(){
+        SQLiteDatabase database=conn.getWritableDatabase();
+        String sql="INSERT INTO "+ TasksUtilities.TABLE_NAME +"("+TasksUtilities.COLUMN_TASK_TITLE+","+TasksUtilities.COLUMN_TASK_DETAILS+") VALUES ('"+task_title+"','"+task_details+"')";
+
+        database.execSQL(sql);
+        database.close();
     }
 }
