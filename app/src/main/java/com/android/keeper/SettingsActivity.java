df9c372e1a20b028b06305b121e7016e9039f912
EGAMAGZ <gazapito1@hotmail.com>
2019-09-23 18:40:46 -0500

    Added SharedPreferences for clock format

--- app/src/main/java/com/android/keeper/SettingsActivity.java
+++ app/src/main/java/com/android/keeper/SettingsActivity.java
@@ -6,18 +6,24 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private Spinner ClockFormat;
+    private Spinner clockFormatSpinner;
     private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedPreEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         sharedPreferences=getSharedPreferences("keeper_settings", Context.MODE_PRIVATE);
+        sharedPreEditor=sharedPreferences.edit();
 
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -25,7 +31,47 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.thirdColor));
+        setSettingsOptions();
 
-        ClockFormat=(Spinner) findViewById(R.id.settings_clock_format_spinner);
+        clockFormatSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String option=parent.getItemAtPosition(position).toString();
+                if(option.equals("Automatic")){
+                    sharedPreEditor.putString("clock_format","auto");
+                    sharedPreEditor.commit();
+                }else if(option.equals("24 Hours")){
+                    sharedPreEditor.putString("clock_format","24hr");
+                    sharedPreEditor.commit();
+                }else if(option.equals("12 Hours")){
+                    sharedPreEditor.putString("clock_format","12hr");
+                    sharedPreEditor.commit();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    private void setSettingsOptions(){
+        String option;
+        clockFormatSpinner=(Spinner) findViewById(R.id.settings_clock_format_spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(getApplicationContext(),R.array.clock_format_options,android.R.layout.simple_spinner_item);
+        clockFormatSpinner.setAdapter(adapter);
+        option=sharedPreferences.getString("clock_format","auto");
+        switch(option){
+            case "auto":
+                clockFormatSpinner.setSelection(0);
+                break;
+            case "24hr":
+                clockFormatSpinner.setSelection(1);
+                break;
+            case "12hr":
+                clockFormatSpinner.setSelection(2);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/android/keeper/dialog/TimePickerDialogFragment.java
+++ app/src/main/java/com/android/keeper/dialog/TimePickerDialogFragment.java
@@ -2,6 +2,8 @@ package com.android.keeper.dialog;
 
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,14 +13,31 @@ import java.text.DateFormat;
 import java.util.Calendar;
 
 public class TimePickerDialogFragment extends DialogFragment {
+
+    private SharedPreferences sharedPreferences;
+    private TimePickerDialog timePickerDialog;
+
+    private String clockFormat;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        sharedPreferences=getContext().getSharedPreferences("keeper_settings", Context.MODE_PRIVATE);
+
+        clockFormat=sharedPreferences.getString("clock_format","auto");
         Calendar calendar=Calendar.getInstance();
         int hour=calendar.get(Calendar.HOUR_OF_DAY);
         int minute=calendar.get(Calendar.MINUTE);
+
         //TODO: ADD SETTINGS OPTION THAT WILL ENABLE OR DISABLE MANUALLY(OR AUTOMATIC FROM SYSTEM) THE 24 FORMAT
+        if(clockFormat.equals("auto")){
+            timePickerDialog= new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(),hour,minute,android.text.format.DateFormat.is24HourFormat(getActivity()));
+        }else if(clockFormat.equals("24hr")){
+            timePickerDialog= new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(),hour,minute,true);
+        }else if(clockFormat.equals("12hr")){
+            timePickerDialog= new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(),hour,minute,false);
+        }
+        return timePickerDialog;
         //return new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(),hour,minute, android.text.format.DateFormat.is24HourFormat(getActivity()));
-        return new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(),hour,minute,true);
     }
 }
