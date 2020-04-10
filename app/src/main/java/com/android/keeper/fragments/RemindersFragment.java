6d9e3dbb4414ffb33b59145f5baf3d0a47ff4ec1
EGAMAGZ <gazapito1@hotmail.com>
2020-04-10 15:15:33 -0500

    Added Adapter to RecyclerView in RemindersFragment

--- app/src/main/java/com/android/keeper/fragments/RemindersFragment.java
+++ app/src/main/java/com/android/keeper/fragments/RemindersFragment.java
@@ -5,7 +5,10 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.keeper.R;
+import com.android.keeper.adapters.RemindersAdapter;
 import com.android.keeper.localdb.SQLiteConnection;
 import com.android.keeper.localdb.utilities.RemindersUtilities;
 import com.android.keeper.recycle_items.ReminderItem;
@@ -24,6 +28,10 @@ public class RemindersFragment extends Fragment {
 
     private View fragmentView;
     private SQLiteConnection sqLiteConnection;
+    private RecyclerView remindersRecyclerView;
+    private RemindersAdapter remindersRecAdapter;
+    private RecyclerView.LayoutManager remindersLayoutManager;
+    private FloatingActionButton addReminderBtn;
 
     private ArrayList<ReminderItem> remindersList;
 
@@ -33,6 +41,18 @@ public class RemindersFragment extends Fragment {
         fragmentView=inflater.inflate(R.layout.fragment_reminders,container,false);
         sqLiteConnection=new SQLiteConnection(getContext(),"keeper_db",null,1);
         remindersList=new ArrayList<ReminderItem>();
+
+        addReminderBtn=fragmentView.findViewById(R.id.addreminder_flt_btn);
+
+        remindersRecyclerView=fragmentView.findViewById(R.id.reminders_recyclerview);
+        remindersLayoutManager=new LinearLayoutManager(getContext());
+
+        addReminderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Crear",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         loadReminders();
 
@@ -46,10 +66,23 @@ public class RemindersFragment extends Fragment {
         try{
             Cursor cursor=database.query(RemindersUtilities.TABLE_NAME,columns,null,null,null,null,
                     RemindersUtilities.COLUMN_REMINDER_ID+" DESC",null);
-
+            while(cursor.moveToNext()){
+                int id=cursor.getInt(0);
+                String title=cursor.getString(1);
+                remindersList.add(new ReminderItem(id,title,false));
+            }
         }catch(Exception e){}
         finally {
+            remindersRecAdapter=new RemindersAdapter(remindersList);
+            remindersRecyclerView.setLayoutManager(remindersLayoutManager);
+            remindersRecyclerView.setAdapter(remindersRecAdapter);
+            remindersRecAdapter.setOnItemClickListener(new RemindersAdapter.OnItemClickListener() {
+                @Override
+                public void onEditReminder(int position) {
 
+                }
+            });
         }
+        database.close();
     }
 }
