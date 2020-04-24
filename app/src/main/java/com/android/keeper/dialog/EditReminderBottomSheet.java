93a4ae85a00f0df83751d36894d160ff6d71e272
EGAMAGZ <gazapito1@hotmail.com>
2020-04-24 13:08:51 -0500

    Added round corners to bottomsheet, and text color to them

---
+++ app/src/main/java/com/android/keeper/dialog/EditReminderBottomSheet.java
@@ -0,0 +1,22 @@
+package com.android.keeper.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditReminderBottomSheet extends BottomSheetDialogFragment {
+
+    private View fragmentView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //fragmentView=inflater.inflate(R.l)
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
